b3189ec7064a4f53452083eb5f7a6dae540b882b
mikrobob <arturmikrik@gmail.com>
2024-03-11 21:47:10 +0300

    Модуль 1 урок 2 задание 3_3

--- module_1/lesson_2/src/solution_3.py
+++ module_1/lesson_2/src/solution_3.py
@@ -2,6 +2,6 @@ books = int(input(" Число книг "))
 stationery = int(input(" Число канцтоваров "))
 toys = int(input(" Число игрушек "))
 
-print(books * 2 + stationery * 1.5 + toys * 3)
+print("Общий обьем: " + str(books * 2 + stationery * 1.5 + toys * 3) + "м^3")
 
 
