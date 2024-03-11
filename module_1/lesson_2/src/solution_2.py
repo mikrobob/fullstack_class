2ee1d1c9544c68d69d928c5b52b97343e6863258
mikrobob <arturmikrik@gmail.com>
2024-03-11 21:35:58 +0300

    Модуль 1 урок 2 задание 3

--- module_1/lesson_2/src/solution_2.py
+++ module_1/lesson_2/src/solution_2.py
@@ -4,4 +4,4 @@ number_2 = int(input("Введите первое число - "))
 number_3 = int(input("Введите первое число - "))
 
 number_sum = int(number_1 + number_2 + number_3)
-print("Среднее арифметическое " + str(int(number_sum / 3)))
+print("Среднее арифметическое " + str(number_sum / 3))
---
+++ module_1/lesson_2/src/solution_3.py
@@ -0,0 +1,7 @@
+books = int(input(" Число книг "))
+stationery = int(input(" Число канцтоваров "))
+toys = int(input(" Число игрушек "))
+
+print(books * 2 + stationery * 1.5 + toys * 3)
+
+
