6e5dd5698161f6f5b59a4a093313679c1a532234
mikrobob <arturmikrik@gmail.com>
2024-03-17 21:45:58 +0300

    Модуль 2 урок 3 задание 1

---
+++ module_2/lesson_3/src/solution_1.py
@@ -0,0 +1,2 @@
+products = [1, 14, 4, 8, 6, 9]
+print(sum(products))
---
+++ module_2/lesson_3/src/solution_2.py
@@ -0,0 +1,8 @@
+worker_1 = 'Маша'
+worker_2 = 'Света'
+worker_3= 'Паша'
+worker_4 = 'Олег'
+
+print("В четные дни работают: ", worker_2, worker_4, "\n"
+      "\n"
+      "В нечетные дни работают: ", worker_1, worker_3)
